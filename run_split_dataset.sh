55f68ac5849bc6585c61ec1b104a77c5f88e48b2
Korbin Ding <jianbind@foxmail.com>
2023-12-19 19:52:58 +0800

    Update shell scripts

--- run_split_dataset.sh
+++ run_split_dataset.sh
@@ -3,13 +3,13 @@ uNames=$(uname -s);
 osName=${uNames:0:10};
 if [ "$osName" = "Darwin" ]; then
   echo "Current system is macOS!";
-  conda activate pytorch;
+  source activate pytorch;
 elif [ "$osName" = "Linux" ]; then
   echo "Current system is Linux!";
-  conda activate pytorch;
+  source /home/korbin/miniconda3/bin/activate pytorch;
 elif [ "$osName" = "MINGW64_NT" ]; then
   echo "Current system is Windows!";
-  source D:/anaconda3/Scripts/activate pytorch;
+  source D:/miniconda3/Scripts/activate pytorch;
 else
   echo "Current system is not supported!";
   exit;
