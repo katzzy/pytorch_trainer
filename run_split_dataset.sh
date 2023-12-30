927f876cbefc0c5a29986289128b5d56f852cddc
Korbin Ding <jianbind@foxmail.com>
2023-12-30 23:05:45 +0800

    chore: update scripts

--- run_split_dataset.sh
+++ run_split_dataset.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
+echo "activate_path: $activate_path";
 uNames=$(uname -s);
 osName=${uNames:0:10};
 if [ "$osName" = "Darwin" ]; then
   echo "Current system is macOS!";
-  conda activate pytorch;
+  source /Users/korbin/anaconda3/bin/activate pytorch;
 elif [ "$osName" = "Linux" ]; then
   echo "Current system is Linux!";
-  conda activate pytorch;
+  source /home/korbin/miniconda3/bin/activate pytorch;
 elif [ "$osName" = "MINGW64_NT" ]; then
   echo "Current system is Windows!";
-  conda activate pytorch;
+  source "D:\miniconda3\Scripts\activate.bat" pytorch;
 else
-  echo "Current system is not supported!";
+  echo "Other system: $osName";
   exit;
 fi
 
 cd ./src/main/ && \
 python ./split_dataset.py \
 --config_file_path ../../configs/split_config.yaml
-#--dataset_dir ../../dataset/paddy-disease-classification/
