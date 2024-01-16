e075a32729234f281603b971638aa7fe9fe5e800
Korbin Ding <jianbind@foxmail.com>
2024-01-16 20:47:30 +0800

    style: :art: format config files and shell scripts

--- run_predict.sh
+++ run_predict.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
-echo "activate_path: $activate_path";
-uNames=$(uname -s);
-osName=${uNames:0:10};
+uNames=$(uname -s)
+osName=${uNames:0:10}
 if [ "$osName" = "Darwin" ]; then
-  echo "Current system is macOS!";
-  source /Users/korbin/anaconda3/bin/activate pytorch;
+  echo "Current system is macOS!"
+  source /Users/korbin/anaconda3/bin/activate pytorch
 elif [ "$osName" = "Linux" ]; then
-  echo "Current system is Linux!";
-  source /home/korbin/miniconda3/bin/activate pytorch;
+  echo "Current system is Linux!"
+  source /home/korbin/miniconda3/bin/activate pytorch
 elif [ "$osName" = "MINGW64_NT" ]; then
-  echo "Current system is Windows!";
-  source "D:\miniconda3\Scripts\activate.bat" pytorch;
+  echo "Current system is Windows!"
+  source "D:\miniconda3\Scripts\activate.bat" pytorch
 else
-  echo "Other system: $osName";
-  exit;
+  echo "Other system: $osName"
+  exit
 fi
 
-
-cd ./src/main/ && \
-python ./predict.py \
---config_file_path ../../configs/eval_config.yaml \
---model_type efficient_model \
---dataset_dir ../../dataset/ \
---submission_file_path ../../submissions/submission.csv
+cd ./src/main/ &&
+  python ./predict.py \
+    --config_file_path ../../configs/eval_config.yaml \
+    --model_type efficient_model \
+    --dataset_dir ../../dataset/ \
+    --submission_file_path ../../submissions/submission.csv
--- run_train.sh
+++ run_train.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-echo "activate_path: $activate_path";
-uNames=$(uname -s);
-osName=${uNames:0:10};
+uNames=$(uname -s)
+osName=${uNames:0:10}
 if [ "$osName" = "Darwin" ]; then
-  echo "Current system is macOS!";
-  source /Users/korbin/anaconda3/bin/activate pytorch;
+  echo "Current system is macOS!"
+  source /Users/korbin/anaconda3/bin/activate pytorch
 elif [ "$osName" = "Linux" ]; then
-  echo "Current system is Linux!";
-  source /home/korbin/miniconda3/bin/activate pytorch;
+  echo "Current system is Linux!"
+  source /home/korbin/miniconda3/bin/activate pytorch
 elif [ "$osName" = "MINGW64_NT" ]; then
-  echo "Current system is Windows!";
-  source "D:\miniconda3\Scripts\activate.bat" pytorch;
+  echo "Current system is Windows!"
+  source "D:\miniconda3\Scripts\activate.bat" pytorch
 else
-  echo "Other system: $osName";
-  exit;
+  echo "Other system: $osName"
+  exit
 fi
 
-if [ ! -d "checkpoints" ];then
-  mkdir checkpoints;
+if [ ! -d "checkpoints" ]; then
+  mkdir checkpoints
 fi
-cd ./src/main/ && \
-python ./train.py \
---config_file_path ../../configs/train_config.yaml \
---epochs 500 \
---batch_size 32 \
---dataset_dir ../../dataset/paddy-disease-classification/ \
---model_type efficient_model \
-| tee ../../checkpoints/output.txt
+cd ./src/main/ &&
+  python ./train.py \
+    --config_file_path ../../configs/train_config.yaml \
+    --epochs 500 \
+    --batch_size 32 \
+    --dataset_dir ../../dataset/paddy-disease-classification/ \
+    --model_type efficient_model |
+  tee ../../checkpoints/output.txt
